fba5882a3b5144c7f1d1c99e4fd369fabfe0dcd0
Mohamed Youssef <myoussef3030@gmail.com>
2022-06-28 08:49:01 +0200

    Setup Tailwind

---
+++ components/Navbar.jsx
@@ -0,0 +1,21 @@
+import classes from '../styles/component-styles/Navbar.module.scss'
+
+
+
+
+const Navbar = () => {
+
+
+
+    return (
+        <div className={classes.Navbar}>
+
+            <h1>Navbar</h1>
+
+        </div>
+    )
+}
+
+export default Navbar
+
+
