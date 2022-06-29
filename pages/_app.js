0bdea81a246985eb20a162b9492da1b22a22e9a9
Mohamed Youssef <myoussef3030@gmail.com>
2022-06-29 15:10:18 +0200

    Create footer component

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps:{session, ...pageProps} }) {
   return (
     <SessionProvider session={session}>
       <Navbar/>  
       <Component {...pageProps} />
+      <Footer/>
     </SessionProvider>
   )
 }
